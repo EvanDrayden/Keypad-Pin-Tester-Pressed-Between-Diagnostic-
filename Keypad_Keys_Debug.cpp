053c2b3fe2b4991806cedd950a98e8d55ea50abf
EvanDrayden <evandrayden@protonmail.com>
2025-11-02 15:16:17 +0530

    Refactor keypad handling by removing debounce logic
    Removed debounce logic and adjusted pin handling for keypad.

--- Keypad_Keys_Debug.cpp
+++ Keypad_Keys_Debug.cpp
@@ -1,7 +1,5 @@
 const int pins[] = {2, 3, 4, 5, 6, 7, 8, 9};  // All keypad pins connected to D2–D9
 const int numPins = sizeof(pins) / sizeof(pins[0]);
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50;
 
 void setup() {
   Serial.begin(9600);
@@ -11,25 +9,18 @@ void setup() {
 }
 
 void loop() {
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    for (int i = 0; i < numPins; i++) {
-      pinMode(pins[i], OUTPUT);
-      digitalWrite(pins[i], LOW);
-      
-      delayMicroseconds(10); // Allow pins to settle
-      
-      for (int j = 0; j < numPins; j++) {
-        if (i != j && digitalRead(pins[j]) == LOW) {
-          Serial.print("Pressed between ");
-          Serial.print(pins[i]);
-          Serial.print(" & ");
-          Serial.println(pins[j]);
-          lastDebounceTime = millis();
-        }
+  for (int i = 0; i < numPins; i++) {
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], LOW);
+    for (int j = 0; j < numPins; j++) {
+      if (i != j && digitalRead(pins[j]) == LOW) {
+        Serial.print("Pressed between ");
+        Serial.print(pins[i]);
+        Serial.print(" & ");
+        Serial.println(pins[j]);
+        delay(400);
       }
-      
-      pinMode(pins[i], INPUT_PULLUP);
-      delayMicroseconds(10); // Allow pins to settle
     }
+    pinMode(pins[i], INPUT_PULLUP);
   }
 }
